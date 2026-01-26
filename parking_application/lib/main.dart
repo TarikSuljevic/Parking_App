ba1c1a5b2ae2efb804abade3f3b3820af62582ea
Kenan Baralic <kenann.bara@gmail.com>
2026-01-26 11:15:53 +0100

    Fix card layout

--- parking_application/lib/main.dart
+++ parking_application/lib/main.dart
@@ -38,36 +38,77 @@ class LoginPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.local_parking, size: 60, color: Colors.deepPurple),
+            Container(
+              padding: EdgeInsets.all(0.1),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.deepPurple, width: 3), // boja i debljina border-a
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                
+              
+              
+              
+                  child: Icon(Icons.local_parking_rounded, size: 60, color: Colors.deepPurple)
+              ),
             ),
             Text('ParkEasy'),
             Text('Pronađite i rezervišite parking mjesto odmah'),
-            Card(
-              margin: EdgeInsets.fromLTRB(150.0, 20, 150, 100),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute<void>(
-                      builder: (context) => UserInputPage())
-                  );
-                },
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 12.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Icon(Icons.supervised_user_circle_rounded, color: Colors.deepPurple, size: 55),
-                      SizedBox(width: 40),
-                      Text('Korisnicka prijava', style: TextStyle(color: Colors.black)),
-                      Text('Pronadite i rezervisite parking mjesta u blizini', style: TextStyle(color: Colors.black54)),
-                    ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Card(
+                   margin: EdgeInsets.symmetric(horizontal: 10,vertical: 20),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute<void>(
+                          builder: (context) => UserInputPage())
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 12.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(Icons.supervised_user_circle_rounded, color: Colors.deepPurple, size: 55),
+                          SizedBox(width: 40),
+                          Text('Korisnicka prijava', style: TextStyle(color: Colors.black)),
+                          Text('Pronadite i rezervisite parking mjesta u blizini', style: TextStyle(color: Colors.black54)),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                  Card(
+                  margin: EdgeInsets.symmetric(horizontal: 10,vertical: 20),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute<void>(
+                          builder: (context) => AdminInputPage())
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 12.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(Icons.supervised_user_circle_outlined, color: Colors.deepPurple, size: 55),
+                          SizedBox(width: 40),
+                          Text('Administratorska prijava', style: TextStyle(color: Colors.black)),
+                          Text('Upravljajte parking objektima i rezervacijama', style: TextStyle(color: Colors.black54)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
+              
           ],
         ),
       ),
@@ -91,12 +132,20 @@ class _UserInputPageState extends State<UserInputPage> {
       ),
       body: Center(
         child: Card(
-          margin: EdgeInsets.fromLTRB(170.0, 20, 170, 100),
+             margin: EdgeInsets.symmetric(horizontal: 560,vertical: 5),
           child: Padding(
-            padding: const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 12.0),
+            padding: const EdgeInsets.fromLTRB(12.0, 100.0, 12.0, 12.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
+                Row(children: [
+         BackButton(
+      // child:
+    
+    
+    ),
+    Text("Nazad"),
+    ],),
                 Icon(Icons.supervised_user_circle_rounded, color: Colors.deepPurple, size: 55),
                 SizedBox(width: 40),
                 Text('Korisnicka prijava', style: TextStyle(color: Colors.black)),
@@ -186,6 +235,97 @@ class _UserInputPageState extends State<UserInputPage> {
     );
   }
 }
+class AdminInputPage extends StatelessWidget {
+  const AdminInputPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      body: Center(
+        child: Card(
+          margin: EdgeInsets.symmetric(horizontal: 560,vertical: 5),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(12.0, 100, 12.0, 12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(children: [
+         BackButton(
+      // child:
+    
+    
+    ),
+    Text("Nazad"),
+    ],),
+                Icon(Icons.supervised_user_circle_outlined, color: Colors.deepPurple, size: 55),
+
+                SizedBox(width: 40),
+                Text('Administratorska prijava', style: TextStyle(color: Colors.black)),
+                Text('Unesite vase podatke za nastavak', style: TextStyle(color: Colors.black54)),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 8.0),
+                        child: Text('Ime'),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Unesite vase ime",
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
+                        child: Text('Email'),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Unesite vas email",
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
+                        child: Text('Lozinka'),
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Unesite vasu lozinku",
+                        ),
+                      ),
+                          Padding(
+                        padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+                        child: Center(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute<void>(
+                                  builder: (context) => Placeholder())
+                              );
+                            }, 
+                            child: Text('Prijavi se')),
+                        ),
+                      ),
+                    ]
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ),
+    );
+  }}
+
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.ime});
@@ -672,4 +812,3 @@ class ParkingSpace {
 
   ParkingSpace({required this.id, required this.status});
 }
-
